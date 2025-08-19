678d52dfb484b87e18086b2e0840371ec6c795d6
pedr7n <pedrotfm68@gmail.com>
2025-08-19 18:00:59 -0300

    Service layer, component registrate

--- src/main/java/com/pedrosouza/springLearning/resources/UserResource.java
+++ src/main/java/com/pedrosouza/springLearning/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.pedrosouza.springLearning.resources;
 
 import com.pedrosouza.springLearning.entities.User;
+import com.pedrosouza.springLearning.services.UserService;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L, "randomName", "randomMail", "111111111", "randomPass111");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+        }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
+
+
